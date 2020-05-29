efb50fa8fe59ed5e0083af5dcdf6cf13dc127d83
Daniel Barter <danielbarter@gmail.com>
2020-05-29 15:13:47 -0700

    local relations stored in a hashtable

--- src/fusion.h
+++ src/fusion.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 typedef uint32_t boundaryLabel;
+typedef uint32_t tileIndex;
 
 /* boundary pattern for tiles
  *    1
@@ -16,20 +17,33 @@ typedef struct tile_s {
 
 // we refer to tiles by their index in the tileStore
 typedef struct tileStore_s {
-  tile *tiles;                // array of tiles
-  uint32_t numberOfTiles;     // number of tile (0 indicates no tiles)
-  uint32_t vacuumTile;        // index of the vacuum tile
+  tile *tiles;                 // array of tiles
+  tileIndex numberOfTiles;     // number of tile (0 indicates no tiles)
+  tileIndex vacuumTile;        // index of the vacuum tile
 } tileStore;
 
 /* boundary pattern for local relations
  *
- *   32
- *  4  1
- *  5  0
- *   67
+ *   3 2
+ *  4   1
+ *  5   0
+ *   6 7
+ *
+ * tile pattern for local relations
+ *
+ *   2 1
+ *   3 0
  *
  */
 
+// local relations are stored in a hash table
 typedef struct localRelation_s {
-  uint32_t boundary[8];       // boundary labels counterclockwise starting 
+  boundaryLabel boundary[8];       // boundary labels counterclockwise
+  tileIndex tiles[4];              // tile indices counterclockwise
+  struct localRelation_s *next;    // next local relation
 } localRelation;
+
+typedef struct localRelationStore_s {
+  uint32_t numberOfBins;
+  localRelation **table;
+} localRelationStore;
