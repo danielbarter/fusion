ee0565eca45dbb63b3d782128b62e64e2dd8f0b1
Daniel Barter <danielbarter@gmail.com>
2020-05-31 16:14:16 -0700

    hashing the seed

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Data.Word (Word8)
 import Foreign.ForeignPtr
 import Foreign.Ptr
+import Foreign.Storable (peek)
 import Data.Vector
+import Crypto.Hash.MD5 (hash)
 import Data.Map.Strict hiding (size)
+import Data.ByteString
+import Data.ByteString.Internal (ByteString(..))
 import qualified Data.Vector.Storable as Storable
 import Codec.Picture
 
@@ -49,21 +55,35 @@ type MacroTile = (Int,Int,Int,Int)
 type LocalRelation = Vector MacroTile
 type LocalRelations = Map MacroBoundary LocalRelation
 
+data Options = Options
+  { seed :: ByteString
+  , numberOfRows :: Int
+  , numberOfColumns :: Int
+  , rowCutoff :: Int
+  , columnCutoff :: Int
+  , tileWidth :: Int
+  , tileHeight :: Int
+  }
 
 foreign import ccall "&freeTeaLeaf" freeTeaLeafC :: FunPtr (Ptr Word8 -> IO ())
 foreign import ccall "generateTeaLeaf" generateTeaLeafC ::
   Int -> Int -> Int -> Int -> Int -> IO (Ptr Word8)
 
 
-generateTeaLeaf :: Int -> Int -> Int -> Int -> Int -> IO (Image Word8)
-generateTeaLeaf seed numRows numCols rowCutoff columnCutoff = do
-  let size = numRows * numCols
-  ptr <- generateTeaLeafC seed numRows numCols rowCutoff columnCutoff
+generateTeaLeaf :: Options -> IO (Image Word8)
+generateTeaLeaf Options{..} = do
+  let size = numberOfRows * numberOfColumns
+      PS hashPtr _ _ = ( hash seed )
+  seedNum <- withForeignPtr ( castForeignPtr hashPtr ) peek
+  ptr <- generateTeaLeafC seedNum
+    numberOfRows numberOfColumns rowCutoff columnCutoff
   foreignPtr <- newForeignPtr freeTeaLeafC ptr
   vector <- Storable.freeze $ Storable.MVector size foreignPtr
-  return $ Image numCols numRows vector
+  return $ Image numberOfColumns numberOfRows vector
+
 
 
 
 main :: IO ()
-main = generateTeaLeaf 34605832 1000 200 20 3 >>= ( writePng "tealeaf.png" )
+main = generateTeaLeaf (Options "hobie" 1000 200 20 3 101 101 ) >>= ( writePng "tealeaf.png" )
+
