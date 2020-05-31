90021a7c7fa71def8daeb1a7ec988b13ac611d90
Daniel Barter <danielbarter@gmail.com>
2020-05-31 13:48:20 -0700

    testing non square tea leaf generator

---
+++ c/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+clang -g tealeaf.c main.c -o runtealeaf.elf -lfftw3 -lm
---
+++ c/main.c
@@ -0,0 +1,8 @@
+#include "tealeaf.h"
+
+
+int32_t main(int32_t argc, char **argv)
+{
+  generateTeaLeaf(34505832,1000,200,20,3);
+  return 0;
+}
