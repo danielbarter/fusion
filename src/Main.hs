ac77c189554d275f60129a3be92f88a9ac8573df
Daniel Barter <danielbarter@gmail.com>
2020-05-31 10:59:32 -0700

    generating a tealeaf in Haskell land

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,20 @@
 
 module Main where
 
-import Foreign.Ptr
+import Data.Int (Int8)
+import Foreign.Marshal.Alloc (free)
+import Data.Primitive.PrimArray
+import Data.Primitive.Ptr
 
-foreign import ccall "generateTeaLeaf" generateTeaLeaf :: Int -> IO (Ptr Bool)
+foreign import ccall "generateTeaLeaf" generateTeaLeafC :: Int -> IO (Ptr Int8)
+
+generateTeaLeaf :: IO (PrimArray Int8)
+generateTeaLeaf = do
+  ptr <- generateTeaLeafC 27
+  teaLeafMutable <- newPrimArray (420 * 420)
+  free ptr
+  copyPtrToMutablePrimArray teaLeafMutable 0 ptr (420 * 420)
+  unsafeFreezePrimArray teaLeafMutable
 
 main :: IO ()
-main = do
-  ptr <- generateTeaLeaf 0
-  return ()
+main = return ()
