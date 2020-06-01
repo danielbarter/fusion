01765ae75f428764b2f39bc3d897040f1047f761
Daniel Barter <danielbarter@gmail.com>
2020-05-31 18:34:38 -0700

    parsing options

--- src/Main.hs
+++ src/Main.hs
@@ -8,13 +8,19 @@ import Data.Word (Word8)
 import Foreign.ForeignPtr
 import Foreign.Ptr
 import Foreign.Storable (peek)
-import Data.Vector
+import qualified Data.Vector as V
 import Crypto.Hash.MD5 (hash)
 import Data.Map.Strict hiding (size)
-import Data.ByteString
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import Data.ByteString.Char8 (unpack)
 import Data.ByteString.Internal (ByteString(..))
+import Data.Csv ((.:))
+import qualified Data.Csv as Csv
 import qualified Data.Vector.Storable as Storable
-import Codec.Picture
+import System.Environment (getArgs)
+import System.Directory (setCurrentDirectory)
+import qualified Codec.Picture as JP
 
 {-
 boundary pattern for tiles
@@ -32,7 +38,7 @@ data Tile = Tile
 
 -- we refer to tiles by their index in the tile store
 data TileStore = TileStore
-  { tiles :: Vector Tile
+  { tiles :: V.Vector Tile
   , vacuumTile :: Int -- usually 0
   }
 
@@ -52,38 +58,63 @@ tile pattern for macro tiles
 
 type MacroBoundary = (Int,Int,Int,Int,Int,Int,Int,Int)
 type MacroTile = (Int,Int,Int,Int)
-type LocalRelation = Vector MacroTile
+type LocalRelation = V.Vector MacroTile
 type LocalRelations = Map MacroBoundary LocalRelation
 
 data Options = Options
   { seed :: ByteString
   , numberOfRows :: Int
   , numberOfColumns :: Int
-  , rowCutoff :: Int
-  , columnCutoff :: Int
+  , rowCutOff :: Int
+  , columnCutOff :: Int
   , tileWidth :: Int
   , tileHeight :: Int
-  }
+  } deriving (Show)
+
+instance Csv.FromNamedRecord Options where
+  parseNamedRecord m = Options <$>
+    m .: "seed" <*>
+    m .: "numberOfRows" <*>
+    m .: "numberOfColumns" <*>
+    m .: "rowCutOff" <*>
+    m .: "columnCutOff" <*>
+    m .: "tileWidth" <*>
+    m .: "tileHeight"
 
 foreign import ccall "&freeTeaLeaf" freeTeaLeafC :: FunPtr (Ptr Word8 -> IO ())
 foreign import ccall "generateTeaLeaf" generateTeaLeafC ::
   Int -> Int -> Int -> Int -> Int -> IO (Ptr Word8)
 
 
-generateTeaLeaf :: Options -> IO (Image Word8)
+generateTeaLeaf :: Options -> IO (JP.Image Word8)
 generateTeaLeaf Options{..} = do
   let size = numberOfRows * numberOfColumns
       PS hashPtr _ _ = ( hash seed )
   seedNum <- withForeignPtr ( castForeignPtr hashPtr ) peek
   ptr <- generateTeaLeafC seedNum
-    numberOfRows numberOfColumns rowCutoff columnCutoff
+    numberOfRows numberOfColumns rowCutOff columnCutOff
   foreignPtr <- newForeignPtr freeTeaLeafC ptr
   vector <- Storable.freeze $ Storable.MVector size foreignPtr
-  return $ Image numberOfColumns numberOfRows vector
+  return $ JP.Image numberOfColumns numberOfRows vector
 
 
 
 
 main :: IO ()
-main = generateTeaLeaf (Options "anna" 1000 200 20 3 101 101 ) >>= ( writePng "tealeaf.png" )
+main = do
+  args <- getArgs
+  if Prelude.length (args) /= 1
+    then Prelude.putStrLn "expecting single argument: tile directory"
+    else do
+      let folder = Prelude.head args
+      setCurrentDirectory folder
+      optionsByteString <- L.readFile "options.csv"
+      let eitherOptions =
+            Csv.decodeByName optionsByteString :: Either String (Csv.Header, V.Vector Options)
+      case eitherOptions of
+        Left err -> putStrLn $ "error parsing options.csv: " <> err
+        Right (_,optionsVector) -> do
+          let options = V.head optionsVector
+          teaLeaf <- generateTeaLeaf options
+          JP.writePng ("tealeaf_" <> (unpack $ seed options) <> ".png") teaLeaf
 
