4590aae099463d7b2cdf2993b457f3ce294223e4
Daniel Barter <danielbarter@gmail.com>
2020-05-31 19:30:55 -0700

    better data types for MacroTiles and MacroBoundaries

--- src/Main.hs
+++ src/Main.hs
@@ -15,7 +15,7 @@ import Data.Map.Strict hiding (size)
 import qualified Data.ByteString.Lazy as L
 import Data.ByteString.Char8 (unpack)
 import Data.ByteString.Internal (ByteString(..))
-import Data.Csv ((.:))
+import Data.Csv ((.:), (.!))
 import qualified Data.Csv as Csv
 import qualified Data.Vector.Storable as Storable
 import System.Environment (getArgs)
@@ -33,14 +33,22 @@ this is the order of the boundary labels in manifest.csv
 
 data Tile = Tile
   { svg :: String
-  , boundary :: (Int,Int,Int,Int)
-  }
+  , right :: Int
+  , top :: Int
+  , left :: Int
+  , bottom :: Int
+  } deriving (Show)
+
+instance Csv.FromRecord Tile where
+  parseRecord m =
+    Tile <$> m .! 0 <*> m .! 1 <*>  m .! 2 <*> m .! 3 <*> m .! 4
+
 
 -- we refer to tiles by their index in the tile store
 data TileStore = TileStore
   { tiles :: V.Vector Tile
   , vacuumTile :: Int -- usually 0
-  }
+  } deriving (Show)
 
 {-
 boundary pattern for macro tiles
@@ -56,8 +64,26 @@ tile pattern for macro tiles
   3 0
 -}
 
-type MacroBoundary = (Int,Int,Int,Int,Int,Int,Int,Int)
-type MacroTile = (Int,Int,Int,Int)
+data MacroBoundary = MacroBoundary
+  { clock4 :: Int
+  , clock2 :: Int
+  , clock1 :: Int
+  , clock11 :: Int
+  , clock10 :: Int
+  , clock8 :: Int
+  , clock7 :: Int
+  , clock5 :: Int
+  }
+
+
+data MacroTile = MacroTile
+  { bottomRight :: Int
+  , topRight :: Int
+  , topLeft :: Int
+  , bottomLeft :: Int
+  }
+
+
 type LocalRelation = V.Vector MacroTile
 type LocalRelations = Map MacroBoundary LocalRelation
 
@@ -69,6 +95,7 @@ data Options = Options
   , columnCutOff :: Int
   , tileWidth :: Int
   , tileHeight :: Int
+  , vacummTile :: Int
   } deriving (Show)
 
 instance Csv.FromNamedRecord Options where
@@ -79,7 +106,8 @@ instance Csv.FromNamedRecord Options where
     m .: "rowCutOff" <*>
     m .: "columnCutOff" <*>
     m .: "tileWidth" <*>
-    m .: "tileHeight"
+    m .: "tileHeight" <*>
+    m .: "vacuumTile"
 
 foreign import ccall "&freeTeaLeaf" freeTeaLeafC :: FunPtr (Ptr Word8 -> IO ())
 foreign import ccall "generateTeaLeaf" generateTeaLeafC ::
@@ -107,7 +135,7 @@ main = do
     then Prelude.putStrLn "expecting single argument: tile directory"
     else do
       let folder = Prelude.head args
-      setCurrentDirectory folder
+      setCurrentDirectory folder -- we work relative to a tile directory
       optionsByteString <- L.readFile "options.csv"
       let eitherOptions = Csv.decodeByName optionsByteString
       case eitherOptions of
@@ -116,4 +144,12 @@ main = do
           let options = V.head optionsVector
           teaLeaf <- generateTeaLeaf options
           JP.writePng ("tealeaf_" <> (unpack $ seed options) <> ".png") teaLeaf
+          manifestByteString <- L.readFile "manifest.csv"
+          let eitherTiles = Csv.decode Csv.NoHeader manifestByteString
+          case eitherTiles of
+            Left err -> putStrLn $ "error parsing manifest.csv: " <> err
+            Right tileVector -> do
+              let tileStore = TileStore tileVector $ vacummTile options
+              putStrLn $ show tileStore
+              return ()
 
